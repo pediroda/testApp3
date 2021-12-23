5e297d7c594a45733cf1f07993ce57c70c070811
pascal <pascal@Mac-de-pascal.local>
2021-12-23 17:38:15 +0100

    Creation

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,33 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var cleanCss = require('gulp-clean-css');
 var rename = require('gulp-rename');
-
+var pump = require('pump');
+var plugins = require('gulp-load-plugins')({
+	rename: {
+		'gulp-angular-templatecache': 'templatecache',
+		'gulp-ng-annotate': 'annotate',
+		'gulp-minify-css': 'minifycss'
+	}
+});
 var paths = {
   sass: ['./scss/**/*.scss']
 };
+
+
+// Working environment
+
+var environment = '';
+var destination = './www/';
+
+gulp.task('env.dev', function() {
+	environment = 'dev';
+});
+
+gulp.task('env.prod', function() {
+	environment = 'prod';
+});
+
+
 
 gulp.task('default', ['sass']);
 
@@ -20,6 +43,39 @@ gulp.task('sass', function(done) {
     .pipe(rename({ extname: '.min.css' }))
     .pipe(gulp.dest('./www/css/'))
     .on('end', done);
+});
+
+// Compile app
+
+gulp.task('js', function(cb) {
+	srcjs = [
+		'./www/js/app.js',
+		'./www/js/*.js',
+	];
+
+	if (process.env['ENV']) {
+		srcjs = srcjs.concat([
+			'!www/src/env~!(' + process.env['ENV'] + ').js',
+		]);
+	} else {
+		srcjs = srcjs.concat([
+			'!www/src/env~!(debug).js',
+		]);
+	};
+
+	srcjs = srcjs.concat([
+		'./wwwlib/*.js',
+	]);
+
+	pump([
+		gulp.src(srcjs),
+		plugins.concat('app.js'),
+		gulp.dest(destination+'compile/'),
+		plugins.annotate({single_quotes: true}),
+		(environment == 'prod' ? plugins.uglify() : plugins.util.noop()),
+		plugins.header(banner),
+		gulp.dest(destination+'compile/')
+	], cb);
 });
 
 gulp.task('watch', ['sass'], function() {
